d10b1fd2f4a20c00ee31912162be370588ce0566
Mikhail <55126412+halloweenja@users.noreply.github.com>
2021-12-22 10:01:32 +0300

    Ui HelpText

--- Assets/Scripts/Ladder/LadderUp.cs
+++ Assets/Scripts/Ladder/LadderUp.cs
@@ -1,20 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LadderUp : MonoBehaviour
 {
     [SerializeField] GameObject _ladderSpawnPoint;
+    [SerializeField] GameObject _helpText;
     private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
+            _helpText.GetComponent<Text>().text = "Press E to use Ladder";
             if (Input.GetKeyDown(KeyCode.E))
             {
                 other.gameObject.GetComponent<CharacterController>().enabled = false;
                 other.gameObject.transform.position = _ladderSpawnPoint.transform.position;
                 other.gameObject.GetComponent<CharacterController>().enabled = true;
+                _helpText.GetComponent<Text>().text = "";
             }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            _helpText.GetComponent<Text>().text = "";
         }
     }
 }
